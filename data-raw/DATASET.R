7d8c539ba0874045eeed9bb63b5045843ab0a94c
tjkerby <tjkerby@gmail.com>
2021-12-15 16:28:35 -0700

    Added LazyDataCompression.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,6 +5,6 @@ split_games <- strsplit(games$moves, split = " ")
 loaded_data <- load_data(split_games)
 
 indexes <- sapply(X=loaded_data, FUN = is.null)
-games_clean <- list.remove(loaded_data, indexes)
+games_clean <- rlist::list.remove(loaded_data, indexes)
 
-usethis::use_data(games_clean, overwrite = TRUE)
+usethis::use_data(games_clean, overwrite = TRUE, compress = "bzip2")
